eb6c16e4a50ec9c31a4f1fc8a5340c6ccd4ad518
Godfrey Leung <godfrey.leung@collinsongroup.com>
2025-12-02 16:32:51 +0000

    feat: modify the get cost breakdown report by using config

--- scripts/cost_monitor/get_aws_bill_by_resouces.py
+++ scripts/cost_monitor/get_aws_bill_by_resouces.py
@@ -6,7 +6,7 @@ import boto3
 import pandas as pd
 import yaml
 
-project_root_directory = Path(__file__).parent.parent
+project_root_directory = Path(__file__).parent.parent.parent
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -63,69 +63,62 @@ if __name__ == "__main__":
 
     start_time = time.time()
 
-    logger.info("Load config ...")
+    logger.info("Load config for cost analysis and breakdown ...")
 
-    with open(project_root_directory / "config/dev.yaml", "r") as f:
-        common_config = yaml.safe_load(f)
+    with open(project_root_directory / "config/cost_monitor.yaml", "r") as f:
+        config = yaml.safe_load(f)
 
-    # project_tags = common_config["project_tag"]
-    # cg_colors = common_config["cg_colors"]
+    start_date = config["start_date"]
+    end_date = config["end_date"]
 
-    # aws_billing_client = boto3.client("billing")
+    granularity = config["Granularity"]
+    metrics_to_report = config["metrics"]
+    filter_to_apply = config["Filter"]
+    groupby_to_apply = config["GroupBy"]
+
+    logger.info("Config loaded.")
+
+    logger.info("Connecting to the AWS Cost Explorer API via boto3 ...")
+
     aws_cost_client = boto3.client("ce", verify=False)
 
     response = aws_cost_client.get_cost_and_usage(
-        TimePeriod={"Start": "2025-01-01", "End": "2025-12-01"},
-        Granularity="MONTHLY",
-        Filter={
-            "Dimensions": {
-                "Key": "SERVICE",
-                "Values": [
-                    "Amazon SageMaker",
-                ],
-                "MatchOptions": ["EQUALS"],
-            }
+        TimePeriod={
+            # "Start": "2025-01-01", "End": "2025-12-01"
+            "Start": start_date, "End": end_date,
         },
-        GroupBy=[
-            {"Type": "TAG", "Key": "Resource"},
-        ],
-        # GroupBy=[
-        #     {
-        #         "Type": "TAG",
-        #         "Key": "Environment"  # Replace with your tag key
-        #     },
-        #     # You can add multiple tags
-        #     # {
-        #     #     "Type": "TAG",
-        #     #     "Key": "Project"
-        #     # },
-        # ],
-        Metrics=["BlendedCost", "UnblendedCost", "UsageQuantity"],
+        Granularity=granularity,
+        Filter=filter_to_apply,
+        GroupBy=groupby_to_apply,
+        Metrics=metrics_to_report,
     )
 
-    # Parse and display results
-    for result in response["ResultsByTime"]:
-        print(
-            f"Time Period: {result['TimePeriod']['Start']} to {result['TimePeriod']['End']}"
-        )
-
-        for group in result["Groups"]:
-            tag_value = group["Keys"][0] if group["Keys"] else "No tag value"
-            blended_cost = group["Metrics"]["BlendedCost"]["Amount"]
-            currency = group["Metrics"]["BlendedCost"]["Unit"]
-
-            print(f"  Tag: {tag_value}")
-            print(f"    Blended Cost: {blended_cost} {currency}")
-            print()
-
+    # TO REMOVE: for inspection/debugging only
     print(response)
-
-    input("......")
+    input("Press to continue ......")
 
     cost_df = format_cost_response_to_dataframe(response)
-    print(cost_df)
 
-    input("......")
+    # TO REMOVE: for inspection/debugging only
+    print(cost_df)
+    input("Press to continue ......")
+
+    logger.info("Cost breakdown report extracted.")
+
+    logger.info("Exporting the cost breakdown report to local CSV file ...")
+
+    export_relative_filepath = config["output_filepath_prefix"]
+
+    relative_filepath = f"{export_relative_filepath}_{start_date}_to_{end_date}.csv"
+    relative_file_dir = project_root_directory / Path(relative_filepath).parent
+
+    if not relative_file_dir.exists():
+        relative_file_dir.mkdir(parents=True, exist_ok=True)
+
+    cost_df.to_csv(
+        project_root_directory / relative_filepath,
+        index=False,
+    )
 
     end_time = time.time()
 
